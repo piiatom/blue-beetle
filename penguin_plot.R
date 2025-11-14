9fc4366273fc1cb69e590f5a81193d6d9d366d7a
piiatom <piia.tomingas4@gmail.com>
2025-11-14 08:54:53 +0000

    added plot title

--- penguin_plot.R
+++ penguin_plot.R
@@ -2,4 +2,4 @@
 summary(palmerpenguins::penguins)
 
 raw_data = palmerpenguins::penguins
-plot(raw_data$flipper_length_mm, raw_data$body_mass_g, xlab = "Flipper length (mm)", ylab = "Body mass (g)")
+plot(raw_data$flipper_length_mm, raw_data$body_mass_g, xlab = "Flipper length (mm)", ylab = "Body mass (g)", main = "The penguin plot")
