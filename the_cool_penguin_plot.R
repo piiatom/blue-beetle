2bb04c0cbcd8f9084ad7b93095e4caa8d806489e
Lea Opitz <55795599+LeaOpitz@users.noreply.github.com>
2025-11-14 12:08:42 +0200

    Lea made plot with ggplot

--- the_cool_penguin_plot.R
+++ the_cool_penguin_plot.R
@@ -1,5 +1,14 @@
 #see the data
 summary(palmerpenguins::penguins)
+library(ggplot2)
+library(viridis)
 
 raw_data = palmerpenguins::penguins
 plot(raw_data$flipper_length_mm, raw_data$body_mass_g, xlab = "Flipper length (mm)", ylab = "Body mass (g)", main = "The penguin plot")
+
+raw_data |> 
+  ggplot(aes(x = flipper_length_mm, y = body_mass_g, col = species)) +
+  geom_point() +
+ # geom_smooth(method = "lm") +
+  labs(x = "Flipper length (mm)", y = "Body mass (g)", title = "The pengin plot") +
+  theme_bw()
